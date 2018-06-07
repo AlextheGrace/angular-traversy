ea1d81876bd451a28991c6cea05464baa44d342f
Alexander Grace <alexander.grace@chasacademy.se>
2018-06-07 12:28:34 +0200

    modding posts components and services

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -2,11 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 
+import { Post } from '../models/Post';
+
 @Injectable()
 export class PostService {
 	postsUrl: string = 'https://jsonplaceholder.typicode.com/posts';
 
 	constructor(private http: HttpClient) {}
 
-	getPosts() {}
+	getPosts(): Observable<Post[]> {
+    return this.http.get<Post[]>(this.postsUrl);
+  }
 }
